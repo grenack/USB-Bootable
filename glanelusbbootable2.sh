a315afb0d4fdedb957812afb4fa78d82624f3efe
Glanel Houenouvi <grenack@gmail.com>
2018-02-13 23:13:04 -0600

    this is my bootable process

---
+++ glanelusbbootable2.sh
@@ -0,0 +1,10 @@
+#Glanel Houenouvi USB Bootable
+#Download Lubuntu 17.0.1
+#Reboot the OS for the changes to take effects
+#setting in virtualbox, go to USB
+#Click on add USB
+#Be sure to choose the right Version of the USB drive
+#Once virtualbox, OS starts, pluggs the USB and Reboot from the Bios
+#Make sure you choose usbboot option
+#On the USB Universal choose Fat32 and the hard drive will be Formated
+#Use Malvastyle-setup to recover the USB.
